3357e9989fee639d0aa0e117acf30bde7e479ad1
Steven Farrell <stevenfarrelltraining@yahoo.com>
2019-09-08 19:23:33 -0400

    Finished the app and got it working fully, have not attempted the stretch goal yet

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,24 +8,55 @@
 
 import Foundation
 
+
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
 }
+
+
 
 class CalculatorBrain {
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
     
-    func addOperandDigit(_ digit: String) -> String {
-        
+    func addOperandDigit(digit: String) -> String {
+        if operatorType == nil {
+            operand1String.append(contentsOf: digit)
+            return operand1String
+        } else {
+            operand2String.append(contentsOf: digit)
+            return operand2String
+        }
     }
-    
-    func setOperator(_ operatorString: String) {
-        
+    func setOperatorType(myOperatorTapped: String) {
+        operatorType = OperatorType(rawValue: myOperatorTapped)
     }
-    
     func calculateIfPossible() -> String? {
-        
+        if operand1String != "" && operand2String != "" {
+            if let operatorType = operatorType {
+                if let operand1Double = Double(operand1String),
+                    let operand2Double = Double(operand2String) {
+                    switch operatorType {
+                    case .addition:
+                        return String(operand1Double + operand2Double)
+                    case .subtraction:
+                        return String(operand1Double - operand2Double)
+                    case .division:
+                        if operand2Double == 0 {
+                            return "Error"
+                        } else {
+                            return String(operand1Double / operand2Double)
+                        }
+                    case .multiplication:
+                        return String(operand1Double * operand2Double)
+                    }
+                }
+            }
+        }
+        return "Error"
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,33 +10,57 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+   
+    var brain: CalculatorBrain?
+    
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
     
+        brain = CalculatorBrain()
+        
+    }
     // MARK: - Action Handlers
-    
-    @IBAction func operandTapped(_ sender: UIButton) {
-        
+    // line 32 is showing the result of our function being ran and showing on storyboard
+    // line 31 is running the entire functionality that we made in our calculatorBrain
+    @IBAction func operandTapped(_ sender: UIButton)
+    {
+        if let numberTapped = sender.titleLabel?.text {
+            if let brain = brain {
+                let output = brain.addOperandDigit(digit: numberTapped)
+                outputLabel.text = output
+            }
+        }
     }
     
-    @IBAction func operatorTapped(_ sender: UIButton) {
-        
+    @IBAction func operatorTapped(_ sender: UIButton)
+    {
+        if let myOperatorTapped = sender.titleLabel?.text {
+            if let brain = brain {
+                brain.setOperatorType(myOperatorTapped: myOperatorTapped)
+            }
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        if let brain = brain {
+            let output = brain.calculateIfPossible()
+            outputLabel.text = output
+        }
     }
     
-    @IBAction func clearTapped(_ sender: UIButton) {
-        
+    @IBAction func clearTapped(_ sender: UIButton? = nil) {
+       clearTransaction()
+        outputLabel.text = "0"
     }
+    
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+     brain = nil
+    brain = CalculatorBrain()
     }
 }
+
